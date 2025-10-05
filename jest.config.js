f340a67d93493f8a692e97f20daa639c545378bd
Piyush K. Das <piyush.k.das25@gmail.com>
2025-10-05 18:23:53 +0530

    fixed jest failing tests

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ export default {
   testEnvironment: 'jsdom',
   setupFiles: ['jest-environment-jsdom'],
   verbose: true,
+  transform: {},
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
 };
